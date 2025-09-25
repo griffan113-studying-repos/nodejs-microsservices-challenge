59b9fa72f6474bb8cd24468cac9e8980ca3f5499
Eliel M. Gaspar <elielmichel@gmail.com>
2025-09-25 10:39:22 -0300

    🎉 Create app-invoices project

---
+++ app-invoices/src/http/server.ts
@@ -0,0 +1,24 @@
+import "../broker/subscriber.ts"
+
+import { fastify } from "fastify"
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod"
+import fastifyCors from "@fastify/cors"
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors, { origin: "*" })
+
+app.get("/health", () => {
+  return { status: "OK" }
+})
+
+app.listen({ host: "0.0.0.0", port: 3002 }).then(() => {
+  console.log("[Invoices] HTTP server running on http://localhost:3002")
+})
